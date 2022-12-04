77e474bc67bc7bf5eca0d766a1061ea2b2ae25e4
Edward Su <edward.su@mail.utoronto.ca>
2022-12-04 17:22:31 -0500

    added profile page

--- PF/frontend/src/App.jsx
+++ PF/frontend/src/App.jsx
@@ -1,24 +1,27 @@
-import './App.css';
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import Layout from './pages/layout'
-import Home from './pages/home';
-import Studios from './pages/studios'
-import Subscrptions from './pages/subscriptions'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Studios from "./pages/studios";
+import Subscrptions from "./pages/subscriptions";
+import Dropdown from "./components/dropdown";
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='studios' element={<Studios />} />
-          <Route path='subscriptions' element={<Subscrptions />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="studios" element={<Studios />} />
+            <Route path="subscriptions" element={<Subscrptions />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-    );
+  );
 }
 
 export default App;
