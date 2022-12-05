1fe7fdf296964972e1bd1e6e0b1a89c943070d0c
Edward Su <edward.su29@gmail.com>
2022-12-05 15:14:06 -0500

    initial commit

--- PF/frontend/src/pages/subscriptions/index.jsx
+++ PF/frontend/src/pages/subscriptions/index.jsx
@@ -3,13 +3,28 @@ import styled from "styled-components";
 const StyledBody = styled.body`
   margin-top: 7%;
 `
-
+const StyledH2 = styled.h2`
+`
+// helper function that will take in the user's subscription data and print it out
+function MembershipGreeting(props){
+  return <h2>Your Membership: ...</h2> 
+  // props.subscription or something will be passed in and then printed
+}
+function NoMembershipGreeting(){
+  return <h2>Select Your TFC Membership</h2>
+}
 function Subscriptions() {
   return (
     <StyledBody>
-      Subscriptions
+      <h1 style={{ textDecoration: 'none', margin: '40px', fontWeight: 'bold', fontFamily: 'Alexandria, sans-serif', fontSize: '3em'}}>Subscriptions</h1>
+
+      {/* get the user's subscription from the backend */}
+      {/* TODO: check if the user currently has a subscription, if so print */}
+      <NoMembershipGreeting style={{ textAlign: 'center' }}/>
+      
     </StyledBody>
     );
 }
+
 
 export default Subscriptions;
