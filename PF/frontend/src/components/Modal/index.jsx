36dcfde7d6bbfd77f15060bf289513d0f628a4ab
Edward Su <edward.su@mail.utoronto.ca>
2022-12-06 15:52:59 -0500

    adding mroe stuff for previous commit

---
+++ PF/frontend/src/components/Modal/index.jsx
@@ -0,0 +1,103 @@
+import React, { useRef, useEffect, useCallback } from "react";
+
+import styled from "styled-components";
+import { MdClose } from "react-icons/md";
+
+// style stuff
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  left: 0;
+  top: 0;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const ModalWrapper = styled.div`
+  width: 700px;
+  height: 200px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  h1 {
+    margin: 10px;
+  }
+  p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+`;
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+export const Modal = ({ showModal, setShowModal, modalMSG, joinFunction }) => {
+  // useref so clicking outside the modal closes it
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  // escape key handler
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {showModal ? (
+        <Background ref={modalRef} onClick={closeModal}>
+          <ModalWrapper showModal={showModal}>
+            <ModalContent>
+              <h1>Your Fitness Journey Starts Here!</h1>
+              <p>{modalMSG}</p>
+              <button onClick={joinFunction}>Join Now</button>
+            </ModalContent>
+            <CloseModalButton
+              aria-label="Close modal"
+              onClick={() => setShowModal((prev) => !prev)}
+            />
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
+};
