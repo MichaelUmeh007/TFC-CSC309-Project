e2a1721e04c07aa9de415757a61a7c0a8e4a73cf
Edward Su <edward.su@mail.utoronto.ca>
2022-12-06 15:40:39 -0500

    added properly working modals with react-toastify messages on join now button, no backend calls / error checks currently

--- PF/frontend/src/pages/subscriptions/index.jsx
+++ PF/frontend/src/pages/subscriptions/index.jsx
@@ -6,11 +6,75 @@ import {
   MembershipPerk,
   StyledH3,
   StyledSpan,
+  StyledButton,
 } from "./Subscriptions.styles";
-
-// styled components
+import { useState } from "react";
+import { Modal } from "../../components/Modal";
+import { GlobalStyle } from "../../globalStyles";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Subscriptions = () => {
+  // toast notifications
+  const notifySubscriptionSuccess = () =>
+    toast.success("Successfully Subscribed!", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  const notifyError = (errorMsg) => {
+    toast.error(errorMsg, {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+
+  const notify = () => {
+    toast("hello");
+  };
+
+  /* TODO: create a hook that stores the state of the user's subscription, with initial value from an API call*/
+
+  //  state for monthly modal
+  const [showMonthlyModal, setshowMonthlyModal] = useState(false);
+  // open modal function
+  const openMonthlyModal = () => {
+    setshowMonthlyModal((prev) => !prev);
+  };
+  // join yearly button handler
+  const handleMonthlySub = () => {
+    // make the API call to change subscription to monthly
+    // handle the credit card + credit card expired + already subscribed case by TODO: figure out how to print out errors
+    // if successful register, call the mysubscription API call, then call setUserSubscription (or whatever) to whatever the API call returns and return a success message
+    notifySubscriptionSuccess();
+  };
+
+  // state for yearly modal
+  const [showYearlyModal, setShowYearlyModal] = useState(false);
+  // open modal function
+  const openYearlyModal = () => {
+    setShowYearlyModal((prev) => !prev);
+  };
+
+  // join yearly button handler
+  const handleYearlySub = () => {
+    // make the API call to change subscription to yearly
+    // handle the credit card + credit card expired + already subscribed case by TODO: figure out how to print out errors
+    // if successful register, call the mysubscription API call, then call setUserSubscription (or whatever) to whatever the API call returns and return a success message
+    notifyError("youre mom");
+  };
+
   return (
     <StyledBody>
       <h1
@@ -24,13 +88,14 @@ const Subscriptions = () => {
       >
         Subscriptions
       </h1>
-
-      {/* TODO: create a hook that stores the state of the user's subscription, with initial value from an API call*/}
+      {/* this needs to be here so toast notifications show up */}
+      <ToastContainer />
       {/* TODO: check the state of the subscription, change  the message accordingly */}
       <NoMembershipGreeting style={{ textAlign: "center" }} />
 
       {/* two column layout for subscriptions */}
       <Row>
+        {/* monthly column */}
         <Column style={{ backgroundColor: "lightgrey" }}>
           <StyledH3>Monthly</StyledH3>
           <div style={{ textAlign: "center" }}>
@@ -46,24 +111,44 @@ const Subscriptions = () => {
             >
               every month
             </h4>
-
             {/* modal for monthly sub */}
 
+            <div className="modal">
+              <StyledButton
+                className="monthly-button"
+                onClick={openMonthlyModal}
+              >
+                Join Now
+              </StyledButton>
+              {/* TODO: add monthly button handler to be passed in */}
+              <Modal
+                showModal={showMonthlyModal}
+                setShowModal={setshowMonthlyModal}
+                modalMSG={"You are subscribing to a monthly membership:"}
+                joinFunction={handleMonthlySub}
+              />
+              <GlobalStyle />
+            </div>
+
             {/* perks */}
-            <MembershipPerk className="monthly-perk">
-              &#10004; Access to all our locations
-            </MembershipPerk>
-            <MembershipPerk className="monthly-perk">
-              &#10004; Access to all our amenities
-            </MembershipPerk>
-            <MembershipPerk className="monthly-perk">
-              &#10004; Access to all our classes
-            </MembershipPerk>
-            <MembershipPerk className="monthly-perk">
-              &#x2717; <s>4.5 free months on us!</s>
-            </MembershipPerk>
+            <div className="perks">
+              <MembershipPerk className="monthly-perk">
+                &#10004; Access to all our locations
+              </MembershipPerk>
+              <MembershipPerk className="monthly-perk">
+                &#10004; Access to all our amenities
+              </MembershipPerk>
+              <MembershipPerk className="monthly-perk">
+                &#10004; Access to all our classes
+              </MembershipPerk>
+              <MembershipPerk className="monthly-perk">
+                &#x2717; <s>4.5 free months on us!</s>
+              </MembershipPerk>
+            </div>
           </div>
         </Column>
+
+        {/* yearly column */}
         <Column style={{ backgroundColor: "crimson", color: "white" }}>
           <StyledH3>Yearly</StyledH3>
           <div style={{ textAlign: "center" }}>
@@ -79,6 +164,21 @@ const Subscriptions = () => {
             >
               every year
             </h4>
+
+            {/* modal */}
+            <div className="modal">
+              <StyledButton className="yearly-button" onClick={openYearlyModal}>
+                Join Now
+              </StyledButton>
+              {/* TODO: add yearly button handler to be passed in */}
+              <Modal
+                showModal={showYearlyModal}
+                setShowModal={setShowYearlyModal}
+                modalMSG={"You are subscribing to a yearly membership:"}
+                joinFunction={handleYearlySub}
+              />
+              <GlobalStyle />
+            </div>
 
             {/* perks */}
             <MembershipPerk className="yearly-perk">
