a140a3982e6699a1d95a6eda01546917a1b467d5
Edward Su <edward.su@mail.utoronto.ca>
2022-12-09 01:09:44 -0500

    youer mom

---
+++ PF/frontend/src/components/Styles/Button.styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+    background-color: black;
+    border: 1px solid black;
+    border-radius: 10px;
+    color: white;
+    padding: 3% 5%;
+    text-align: center;
+    font-weight: bold;
+    text-decoration: none;
+    margin: 4%;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #313332;
+        color: white;
+        border: 1px solid black;
+    }
+`
