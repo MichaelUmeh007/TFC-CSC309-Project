9ac5ae4b68f1b675bc32aac58c1d92cc9569c0c9
Edward Su <edward.su@mail.utoronto.ca>
2022-12-09 14:09:41 -0500

    fixing some == warnings

--- PF/frontend/src/components/SubscriptionContext/CurrentPlanText.jsx
+++ PF/frontend/src/components/SubscriptionContext/CurrentPlanText.jsx
@@ -8,29 +8,31 @@ const url = "http://127.0.0.1:8000";
 const path = "/subscriptions/my-subscription/";
 
 function App() {
-    const [post, setPost] = useState([]);
-    const authheader = useAuthHeader();
-    const config = {
-        headers: {
-            "Content-Type": "application/json", 
-            Authorization: `${authheader()}`,
-            withCredentials: false
-        }
-    }
-    useEffect(() => {
-        axios.get(`${url}${path}`, config).then((response) => {
-            setPost(response.data);
-        });
-    }, []);
+  const [post, setPost] = useState([]);
+  const authheader = useAuthHeader();
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${authheader()}`,
+      withCredentials: false,
+    },
+  };
+  useEffect(() => {
+    axios.get(`${url}${path}`, config).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
 
-    if (post["subscription_cost"] == undefined) {
-        return (<StyledNoPlanTitle> Not Currently Subscribed </StyledNoPlanTitle>);
-    }
-    else {
-        return (
-            <StyledPlanTitle> { "$" + post["subscription_cost"] + "/" + post["subscription"] } </StyledPlanTitle>
-        );
-    }
+  if (post["subscription_cost"] === undefined) {
+    return <StyledNoPlanTitle> Not Currently Subscribed </StyledNoPlanTitle>;
+  } else {
+    return (
+      <StyledPlanTitle>
+        {" "}
+        {"$" + post["subscription_cost"] + "/" + post["subscription"]}{" "}
+      </StyledPlanTitle>
+    );
+  }
 }
 
 export default App;
